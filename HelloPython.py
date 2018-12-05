fb9b516ab62b286213dd84bd2f3d2226ba065f30
Sean O Connor <s.m.oconnor@cit.ie>
2018-12-05 15:48:47 +0000

    third change

--- HelloPython.py
+++ HelloPython.py
@@ -5,5 +5,6 @@ print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
 print ("first change for trigger")	
 print ("second change for trigger")
+print ("third change for trigger")
 
 	
