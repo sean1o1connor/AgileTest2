660241080885b7155663d7550b2eff7597a58915
Sean O Connor <s.m.oconnor@cit.ie>
2018-12-05 15:31:37 +0000

    Update HelloPython.py

---
+++ HelloPython.py
@@ -0,0 +1,9 @@
+import datetime
+print("Hello world!")
+now = datetime.datetime.now()
+print ("Current date and time is ")
+print (now.strftime("%A, %d-%m-%Y : %H:%M"))
+print ("first change for trigger")	
+print ("second change for trigger")
+
+	
